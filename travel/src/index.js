cd799f87e9222171386ef7696b631b3ebc907305
bruteforce91 <corsarosalvatore91@yahoo.it>
2020-11-26 11:58:17 +0000

    chiamata API

--- travel/src/index.js
+++ travel/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Api from './Api/Api'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+  <div>
+    <Api />
+  </div>,
   document.getElementById('root')
 );
 
