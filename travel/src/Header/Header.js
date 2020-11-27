2b5de16ee4b2966bcaf454eea301d8581408d206
giorgioita91 <giorgioita91@gmail.com>
2020-11-27 15:43:49 +0100

    header completato

--- travel/src/Header/Header.js
+++ travel/src/Header/Header.js
@@ -1,24 +1,30 @@
-import React, { useContext } from 'react'
-import {ContextApi} from '../Api/ContextApi'
-
+import React, { useContext } from "react";
+import { ContextApi } from "../Api/ContextApi";
 
 function Header() {
-    const dati = useContext(ContextApi)
-    return (
-        <>
-            <div className="container-fluid" id="header">
-                <div className="row">
-                    <div className="col">
-                        {console.log("ture:",dati)}
-                        <h1 id="titleHeader">{dati.title}</h1>
-                        <img id="logo-agency" src={dati.agency.image}></img>
-                        <img src={dati.images[0].image}></img>
-                    </div>
-                </div>
-                
-            </div>
-        </>
-    )
+  const dati = useContext(ContextApi);
+  return (
+    <>
+      <div
+        className="container-fluid"
+        id="header"
+        style={{ backgroundImage: `url(${dati.images[0].image})` }}
+      >
+        <div className="row">
+          <div className="col">
+            {console.log("ture:", dati)}
+            <h1 id="titleHeader">{dati.title}</h1>
+            <img id="logoAgency" src={dati.agency.image}></img>
+            <p id="button">
+              <button type="button" className="btn btn-primary">
+                scopri di più
+              </button>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Header
+export default Header;
