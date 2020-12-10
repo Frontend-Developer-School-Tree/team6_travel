caaae60af263447d0c6f061931e4d62af1e1cfc3
bruteforce91 <corsarosalvatore91@yahoo.it>
2020-12-10 16:21:18 +0100

    modal in siracusa2

--- travel/src/Cards/ComponentTappe/CardSiracusa2.js
+++ travel/src/Cards/ComponentTappe/CardSiracusa2.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 import "../../assets/css/CardSiracusa2.css";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 function CardSiracusa2(props) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   const [data, setData] = useState(props.value);
   return (
     <div className="container-fluid accordition-print">
@@ -103,12 +110,12 @@ function CardSiracusa2(props) {
                               {data.data1S2}
                             </p>
                           </div>
-                            <div
-                              className="background-image h-300"
-                              style={{
-                                backgroundImage: `url(${data.img1S2})`,
-                              }}
-                            ></div>
+                          <div
+                            className="background-image h-300"
+                            style={{
+                              backgroundImage: `url(${data.img1S2})`,
+                            }}
+                          ></div>
                           <div className="card-body pt-0">
                             <div className="no-print">
                               <div className="VueCarousel d-block d-lg-none">
@@ -225,7 +232,7 @@ function CardSiracusa2(props) {
                                 </div>
                               </div>
                               {/** immagini sotto il paragrafo Alternativa 1 */}
-                              <div className="row pl-4">
+                              <div className="row pl-4" onClick={handleShow}>
                                 <div className="col-12">
                                   <div className="container-gallery">
                                     <div
@@ -336,7 +343,7 @@ function CardSiracusa2(props) {
                             </div>
                             <div className="col-12 col-xl-6">
                               <div style={{ breakInside: "avoid" }}>
-                                <div className="col-12 hover-section p-4">
+                                <div className="col-12 hover-section p-4" onClick={handleShow}>
                                   <div>
                                     <p className="rob title-hotel title-section font-weight-bold">
                                       Tenuta San Michele
@@ -366,117 +373,127 @@ function CardSiracusa2(props) {
                                     </p>
                                   </div>
                                 </div>
-                                <div
-                                  aria-hidden="true"
-                                  className="modal custom-modal"
-                                >
+                                <div className="modal custom-modal ">
+                                  <div className="modal-dialog modal-dialog-centered modal-xl">
+                                    <Modal show={show} onHide={handleClose}>
+                                      <Modal.Header closeButton>
+                                        <Modal.Title>
+                                          Tenuta San Michele
+                                        </Modal.Title>
+                                      </Modal.Header>
+                                      <Modal.Body>
+                                        <>
+                                              <div className="row my-3">
+                                                <div className="col-12 rob small-text">
+                                                  <p className="rob">
+                                                    Agriturismo
+                                                  </p>
+                                                </div>
+                                              </div>
+                                              <div className="container-img">
+                                                <div
+                                                  className="image"
+                                                  style={{
+                                                    backgroundImage: `url(${data.img1Pargrafo})`,
+                                                  }}
+                                                ></div>
+                                                <div
+                                                  className="image"
+                                                  style={{
+                                                    backgroundImage: `url(${data.img2Pargrafo})`,
+                                                  }}
+                                                ></div>
+                                                <div
+                                                  className="image"
+                                                  style={{
+                                                    backgroundImage: `url(${data.img3Pargrafo})`,
+                                                  }}
+                                                ></div>
+                                                <div className="row mt-4 w-100">
+                                                  <p className="col-12 small-text rob text-uppercase font-weight-bold">
+                                                    Descrizione
+                                                  </p>
+                                                  <div className="col-12 rob small-text">
+                                                    l'agritourisme appartient au
+                                                    cellier renommé Tenuta San
+                                                    Michele du Baron Emanuele
+                                                    Scammacca del Murgo, où sont
+                                                    produits des vins typiques
+                                                    du terroir et un vin
+                                                    mousseux. Possibilité de
+                                                    participer aux activités du
+                                                    cellier, dégustation de vins
+                                                    et visite des caves avec
+                                                    guide.
+                                                  </div>
+                                                </div>
+                                                <div className="row mt-4 w-100">
+                                                  <p className="col-12 small-text rob text-uppercase font-weight-bold">
+                                                    Ristorante
+                                                  </p>
+                                                  <div className="col-12 rob small-text">
+                                                    petit déjeuner servi au
+                                                    restaurant ou en terrasse.
+                                                    Le restaurant propose des
+                                                    plats typiquement siciliens.
+                                                  </div>
+                                                </div>
+                                                <div className="row mt-4 w-100">
+                                                  <p className="col-12 small-text rob text-uppercase font-weight-bold">
+                                                    Camere
+                                                  </p>
+                                                  <div className="col-12 rob small-text">
+                                                    agréables, avec salle de
+                                                    bain ou douche/WC,
+                                                    sèche-cheveux, chauffage,
+                                                    Wi-Fi (gratuit), TV via
+                                                    satellite, bouilloire
+                                                    électrique, téléphone,
+                                                    mini-bar (payant). Certaines
+                                                    chambres sont climatisées.
+                                                  </div>
+                                                </div>
+                                                <div className="row mt-4 w-100">
+                                                  <p className="col-12 small-text rob text-uppercase font-weight-bold">
+                                                    Servizi
+                                                  </p>
+                                                  <div className="col-12 rob small-text">
+                                                    piscine panoramique.
+                                                    Diverses excursions
+                                                    organisées sur demande
+                                                    (payant).
+                                                  </div>
+                                                </div>
+                                                <div className="row mt-4 w-100">
+                                                  <div className="col-12 rob small-text">
+                                                    <p className="rob normal-text mt-2">
+                                                      Web:{" "}
+                                                      <a href="www.hotel.ie">
+                                                        www.hotel.ie
+                                                      </a>
+                                                    </p>
+                                                  </div>
+                                                </div>
+                                              </div>
+                                            
+                                        </>
+                                      </Modal.Body>
+                                      <Modal.Footer>
+                                        <Button
+                                          variant="secondary"
+                                          onClick={handleClose}
+                                        >
+                                          Close
+                                        </Button>
+                                      </Modal.Footer>
+                                    </Modal>
+                                  </div>
+                                </div>
+                                <div className="modal custom-modal">
                                   <div
                                     role="document"
                                     className="modal-dialog modal-dialog-centered modal-lg"
-                                  >
-                                    <div className="modal-content">
-                                      <div className="modal-header">
-                                        <h5 className="modal-title rob">
-                                          Tenuta San Michele
-                                        </h5>
-                                        <button
-                                          type="button"
-                                          aria-label=""
-                                          className="close"
-                                        >
-                                          <span aria-hidden="true">×</span>
-                                        </button>
-                                      </div>
-                                      <div className="modal-body">
-                                        <div className="row my-3">
-                                          <div className="col-12 rob small-text">
-                                            <p className="rob">Agriturismo</p>
-                                          </div>
-                                        </div>
-                                        <div className="container-img">
-                                          <div
-                                            className="image"
-                                            style={{
-                                              backgroundImage: `url(${data.img1Pargrafo})`,
-                                            }}
-                                          ></div>
-                                          <div
-                                            className="image"
-                                            style={{
-                                              backgroundImage: `url(${data.img2Pargrafo})`,
-                                            }}
-                                          ></div>
-                                          <div
-                                            className="image"
-                                            style={{
-                                              backgroundImage: `url(${data.img1Pargrafo})`,
-                                            }}
-                                          ></div>
-                                          <div className="row mt-4 w-100">
-                                            <p className="col-12 small-text rob text-uppercase font-weight-bold">
-                                              Descrizione
-                                            </p>
-                                            <div className="col-12 rob small-text">
-                                              l'agritourisme appartient au
-                                              cellier renommé Tenuta San Michele
-                                              du Baron Emanuele Scammacca del
-                                              Murgo, où sont produits des vins
-                                              typiques du terroir et un vin
-                                              mousseux. Possibilité de
-                                              participer aux activités du
-                                              cellier, dégustation de vins et
-                                              visite des caves avec guide.
-                                            </div>
-                                          </div>
-                                          <div className="row mt-4 w-100">
-                                            <p className="col-12 small-text rob text-uppercase font-weight-bold">
-                                              Ristorante
-                                            </p>
-                                            <div className="col-12 rob small-text">
-                                              petit déjeuner servi au restaurant
-                                              ou en terrasse. Le restaurant
-                                              propose des plats typiquement
-                                              siciliens.
-                                            </div>
-                                          </div>
-                                          <div className="row mt-4 w-100">
-                                            <p className="col-12 small-text rob text-uppercase font-weight-bold">
-                                              Camere
-                                            </p>
-                                            <div className="col-12 rob small-text">
-                                              agréables, avec salle de bain ou
-                                              douche/WC, sèche-cheveux,
-                                              chauffage, Wi-Fi (gratuit), TV via
-                                              satellite, bouilloire électrique,
-                                              téléphone, mini-bar (payant).
-                                              Certaines chambres sont
-                                              climatisées.
-                                            </div>
-                                          </div>
-                                          <div className="row mt-4 w-100">
-                                            <p className="col-12 small-text rob text-uppercase font-weight-bold">
-                                              Servizi
-                                            </p>
-                                            <div className="col-12 rob small-text">
-                                              piscine panoramique. Diverses
-                                              excursions organisées sur demande
-                                              (payant).
-                                            </div>
-                                          </div>
-                                          <div className="row mt-4 w-100">
-                                            <div className="col-12 rob small-text">
-                                              <p className="rob normal-text mt-2">
-                                                Web:{" "}
-                                                <a href="www.hotel.ie">
-                                                  www.hotel.ie
-                                                </a>
-                                              </p>
-                                            </div>
-                                          </div>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
+                                  ></div>
                                 </div>
                               </div>
                             </div>
